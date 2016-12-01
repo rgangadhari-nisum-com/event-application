5ac0f797dd0b527f619964d12a3bed538153f6bb
rajini <rajini@192.168.1.143>
2016-12-01 17:08:09 +0530

    test

---
+++ event-application/src/main/java/com/event/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.event.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	public String getLoginData(){
+		return "HIII hello";
+	}
+
+}
